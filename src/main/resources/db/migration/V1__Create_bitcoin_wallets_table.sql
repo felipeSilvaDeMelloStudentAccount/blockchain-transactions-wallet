f857bbd00bc0da51670145355edd1ed9c197f5d7
unknown <d23125661@mytudublin.ie>
2024-05-12 19:01:38 +0100

    working

--- src/main/resources/db/migration/V1__Create_bitcoin_wallets_table.sql
+++ src/main/resources/db/migration/V1__Create_bitcoin_wallets_table.sql
@@ -1,8 +1,10 @@
-CREATE TABLE IF NOT EXISTS bitcoin_wallets (id SERIAL PRIMARY KEY, address VARCHAR(255),
+CREATE TABLE IF NOT EXISTS bitcoin_wallets (
+                                               id BIGSERIAL PRIMARY KEY,
+                                               address VARCHAR(255),
     balance BIGINT,
     private_key VARCHAR(255),
     public_key VARCHAR(255),
     seed_words TEXT,
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+    );
