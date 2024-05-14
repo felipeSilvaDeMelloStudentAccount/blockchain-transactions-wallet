48424a73877c82b81d2a462ce4d985b546e94ba8
unknown <d23125661@mytudublin.ie>
2024-05-14 20:36:24 +0100

    Pagination to get All blocks

--- src/main/java/com/fsdm/bitcoinbj/service/BlockService.java
+++ src/main/java/com/fsdm/bitcoinbj/service/BlockService.java
@@ -1,20 +1,19 @@
 package com.fsdm.bitcoinbj.service;
 
-import com.fsdm.bitcoinbj.model.resource.TransactionResource;
 import com.fsdm.bitcoinbj.model.resource.BlockResource;
+import com.fsdm.bitcoinbj.model.resource.TransactionResource;
 import com.fsdm.bitcoinbj.model.transaction.BlockDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 
 import java.util.List;
 
 public interface BlockService {
-    List<BlockDAO> getAllBlocks();
-
-    BlockDAO getBlockByHash(String hash);
 
     EntityModel<BlockResource> getBlockResourceByHash(String hash);
 
     List<TransactionResource> getTransactionsByBlockHash(String hash);
+
+    Page<BlockDAO> getAllBlocks(Pageable pageable);
 }
-
-
