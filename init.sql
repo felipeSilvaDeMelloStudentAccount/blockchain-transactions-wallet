fbf2aa602ba84eef9d9da4930d42dac082c73d19
unknown <d23125661@mytudublin.ie>
2024-05-18 22:10:06 +0100

    Google Style Format

--- init.sql
+++ init.sql
@@ -1,19 +1,23 @@
-DO $$
+DO
+$$
     BEGIN
         -- Check if the database does not exist
         IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'blockchain_db') THEN
             -- Create the database if it does not exist
             CREATE DATABASE blockchain_db;
         END IF;
-    END $$;
+    END
+$$;
 
 
 -- Grant privileges to the user
 GRANT ALL PRIVILEGES ON DATABASE blockchain_db TO blockchain;
 
 -- Drop constraints if they exist
-ALTER TABLE IF EXISTS "transaction_inputs" DROP CONSTRAINT IF EXISTS fk_inputs;
-ALTER TABLE IF EXISTS "transaction_outputs" DROP CONSTRAINT IF EXISTS fk_outputs;
+ALTER TABLE IF EXISTS "transaction_inputs"
+    DROP CONSTRAINT IF EXISTS fk_inputs;
+ALTER TABLE IF EXISTS "transaction_outputs"
+    DROP CONSTRAINT IF EXISTS fk_outputs;
 
 -- Drop tables if they exist
 DROP TABLE IF EXISTS "transaction_inputs" CASCADE;
@@ -22,33 +26,37 @@ DROP TABLE IF EXISTS "transactions" CASCADE;
 DROP TABLE IF EXISTS "blocks" CASCADE;
 
 -- Create the "blocks" table
-CREATE TABLE IF NOT EXISTS "blocks" (
-                                        hash VARCHAR PRIMARY KEY,
-                                        previous_hash VARCHAR,
-                                        nonce BIGINT,
-                                        difficulty BIGINT,
-                                        timestamp TIMESTAMPTZ
+CREATE TABLE IF NOT EXISTS "blocks"
+(
+    hash          VARCHAR PRIMARY KEY,
+    previous_hash VARCHAR,
+    nonce         BIGINT,
+    difficulty    BIGINT,
+    timestamp     TIMESTAMPTZ
 );
 
 -- Create the "transactions" table
-CREATE TABLE IF NOT EXISTS "transactions" (
-                                              transaction_id VARCHAR PRIMARY KEY,
-                                              block_hash VARCHAR REFERENCES "blocks"(hash)
+CREATE TABLE IF NOT EXISTS "transactions"
+(
+    transaction_id VARCHAR PRIMARY KEY,
+    block_hash     VARCHAR REFERENCES "blocks" (hash)
 );
 
 -- Create the "transaction_inputs" table
-CREATE TABLE IF NOT EXISTS "transaction_inputs" (
-                                                    id SERIAL PRIMARY KEY,
-                                                    source_transaction_id VARCHAR,
-                                                    output_index INT,
-                                                    script_sig VARCHAR,
-                                                    transaction_id VARCHAR REFERENCES "transactions"(transaction_id)
+CREATE TABLE IF NOT EXISTS "transaction_inputs"
+(
+    id                    SERIAL PRIMARY KEY,
+    source_transaction_id VARCHAR,
+    output_index          INT,
+    script_sig            VARCHAR,
+    transaction_id        VARCHAR REFERENCES "transactions" (transaction_id)
 );
 
 -- Create the "transaction_outputs" table
-CREATE TABLE IF NOT EXISTS "transaction_outputs" (
-                                                     id SERIAL PRIMARY KEY,
-                                                     value DECIMAL,
-                                                     script_pub_key VARCHAR,
-                                                     transaction_id VARCHAR REFERENCES "transactions"(transaction_id)
+CREATE TABLE IF NOT EXISTS "transaction_outputs"
+(
+    id             SERIAL PRIMARY KEY,
+    value          DECIMAL,
+    script_pub_key VARCHAR,
+    transaction_id VARCHAR REFERENCES "transactions" (transaction_id)
 );
